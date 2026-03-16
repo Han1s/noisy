3acbfb4d098f6bed6750074db3d620639c1013d6
Jan Ranostaj <HonzaRanostaj@gmail.com>
2026-03-16 23:23:22 +0100

    add basic card

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,14 @@
+import YouTube, { type YouTubeProps } from 'react-youtube'
 import { createFileRoute } from '@tanstack/react-router'
 import { createServerFn } from '@tanstack/react-start'
-import { Button } from '@mantine/core'
+import { AspectRatio, Card, Grid } from '@mantine/core'
+import { MOCK_VIDEOS } from '../MOCK_DATA'
+
+const getYoutubeId = (url: string) => {
+  const regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|&v=)([^#&?]*).*/
+  const match = url.match(regExp)
+  return match && match[2].length === 11 ? match[2] : null
+}
 
 const serverLoader = createServerFn({ method: 'GET' }).handler(() => {
   return { name: 'test' }
@@ -14,7 +22,24 @@ export const Route = createFileRoute('/')({
 })
 
 function App() {
-  const data = Route.useLoaderData()
+  const opts: YouTubeProps['opts'] = {
+    height: '100%',
+    width: '100%',
+  }
 
-  return <Button variant="filled">Button {data.name}</Button>
+  return (
+    <Grid>
+      {MOCK_VIDEOS.map((videoId) => {
+        return (
+          <Grid.Col span={3}>
+            <Card key={videoId} shadow="sm" padding="0" radius="md" withBorder>
+              <Card.Section>
+                <YouTube id={videoId} videoId={videoId} opts={opts} />
+              </Card.Section>
+            </Card>
+          </Grid.Col>
+        )
+      })}
+    </Grid>
+  )
 }
